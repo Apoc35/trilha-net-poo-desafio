5c3e88cdd5d13de8fd8f3622608e41a22e28be89
Apoc35 <alexv_silveira@outlook.com>
2024-01-06 20:20:03 -0300

    fix: Não havia feito o fork, apenas baixei direto do desafio. Tive que fazer novamente colando do anterior.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
+// TODO: Realizar os testes com as classes Nokia e Iphoneusing DesafioPOO.Models;
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone cel = new Iphone("123456","N2530","11111979111", 64);
+cel.Ligar();
+cel.ReceberLigacao();
+cel.InstalarAplicativo("whatsup");
+
+Console.WriteLine("\n");
+
+Nokia cel2 = new Nokia("123456","N2530","11111979111", 64);
+cel2.Ligar();
+cel2.ReceberLigacao();
+cel2.InstalarAplicativo("whatsup");
